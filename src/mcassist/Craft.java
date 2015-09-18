990a68e984a48a9eb645bccba1442067cd36ff9c
AlexisM <alexis@marquis.fr.cr>
2015-09-18 08:18:31 +0200

    First commit

---
+++ src/mcassist/Craft.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mcassist;
+
+/**
+ *
+ * @author alexis
+ */
+public class Craft {
+    
+    private Item[][] items;
+    
+    public Craft() {
+        this.items = new Item[3][3];
+    }
+    
+}
---
+++ src/mcassist/Item.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mcassist;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alexis
+ */
+public class Item {
+    
+    private String name;
+    private ArrayList<Craft> crafts;
+    
+    public Item(String name) {
+        this.name = name;
+        this.crafts = new ArrayList();
+    } 
+    
+    public void addCraft(Craft craft) {
+        this.crafts.add(craft);
+    } 
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public ArrayList getCrafts() {
+        return this.crafts;
+    }
+    
+    public boolean isRessource() {
+        return this.crafts.isEmpty();
+    }
+            
+}
